c389b96e8c18a40f821f3e916a81c8782de13f24
Son Heegwan <gmlrhks95@gmail.com>
2021-05-22 21:58:01 +0900

    StockNews modified

--- src/StockNews.java
+++ src/StockNews.java
@@ -1,44 +1,93 @@
 import java.awt.Component;
-
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
 
 public class StockNews {
-	private JPanel newsPanel;
+	private JScrollPane newsScorllPane;
+	private JPanel newsMainPanel;
 	private NewsDataModel newsModel;
 	private SearchPane parentPane;
-	
+	private final int displayNews = 10;
+	private NewsPanel[] newsPanes;
+
 	StockNews(final SearchPane searchPane) {
-		newsModel = new NewsDataModel(this);
 		NewsDataController newsDataEventListener = new NewsDataController(this, newsModel);
 		
-		newsPanel = new JPanel();
-		newsPanel.setAlignmentY(Component.LEFT_ALIGNMENT);
-		newsPanel.setLayout(new BoxLayout(newsPanel, BoxLayout.Y_AXIS));
+		newsMainPanel = new JPanel();
+		newsMainPanel.setLayout(new BoxLayout(newsMainPanel, BoxLayout.Y_AXIS));
+		newsMainPanel.setAlignmentY(Component.LEFT_ALIGNMENT);
+		newsMainPanel.setMaximumSize(new Dimension(860, 350));
+
+		newsScorllPane = new JScrollPane(newsMainPanel,
+			JScrollPane.VERTICAL_SCROLLBAR_ALWAYS ,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		
 		searchPane.searchResults.addMouseListener(newsDataEventListener);
 		searchPane.stockBookmark.addMouseListener(newsDataEventListener);
 		parentPane = searchPane;
+
+		newsPanes = new NewsPanel[displayNews];
+		for (int i = 0; i < displayNews; i++) {
+			newsPanes[i] = new NewsPanel();
+			newsMainPanel.add(newsPanes[i]);
+		}
 	}
 	
-	public JPanel getPanel() {
-		return newsPanel;
+	public JScrollPane getPanel() {
+		return newsScorllPane;
 	}
 
 	public String getStockName() {
 		return parentPane.getStockName();
 	}
 
-	void updateNewsData(String[] titles, String[] links, int display) {
-		newsPanel.removeAll();
-		newsPanel.revalidate();
-		newsPanel.repaint();
-
-		JLabel curLabel;
-		for (int i = 0; i < display; i++) {
-			curLabel = new JLabel(titles[i]);
-			newsPanel.add(curLabel);
+	void updateNewsData(NewsDataModel model) {
+		for (int i = 0; i < model.display; i++) {
+			String title = model.items[i].title;
+			String desc = model.items[i].description;
+			String date = model.items[i].pubDate;
+			newsPanes[i].updateNews(title, desc, date);
 		}
+	}
+
+	class NewsPanel extends JPanel {
+		private JLabel dateLbl;
+		private Date date;
+		private JTextArea titleTA;
+		private String title;
+		private JTextArea descTA;
+		private String desc;
+
+		NewsPanel() {
+			setAlignmentY(Component.LEFT_ALIGNMENT);
+			setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+			Font NanumFont = FontController.loadFontFile("NanumBarunGothic.ttf");
+			Font NanumBoldFont = FontController.loadFontFile("NanumBarunGothicBold.ttf");
+			FontController.registerFont(NanumFont);
+
+			Font NanumTitleFont = NanumBoldFont.deriveFont(16f);
+			titleTA = new JTextArea();
+			titleTA.setFont(NanumTitleFont);
+			titleTA.setEditable(false);
+			titleTA.setWrapStyleWord(true);
+			titleTA.setLineWrap(true);
+
+			Font NanumDescFont = NanumFont.deriveFont(12f);
+			descTA = new JTextArea();
+			descTA.setFont(NanumDescFont);
+			descTA.setEditable(false);
+			descTA.setWrapStyleWord(true);
+			descTA.setLineWrap(true);
+
+			add(titleTA);
+			add(descTA);
+		}
+
+		public void updateNews(String title, String desc, String date) {
+			titleTA.setText(title);
+			descTA.setText(desc);
+		}
+		
 	}
 }
